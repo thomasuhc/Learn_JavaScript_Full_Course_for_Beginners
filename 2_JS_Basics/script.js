af80237ca0aa6f6fc6c57b31deca3ffcfa8d338b
Thomas Chu <thomasuhc@gmail.com>
2020-09-17 14:10:14 -0400

    Basic JS
    Update code

---
+++ 2_JS_Basics/script.js
@@ -0,0 +1,73 @@
+var firstName = 'Anna';
+console.log(firstName);
+
+var lastName = 'Paula';
+var age = 25;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
+var SerenaLi = 'Serena Li';
+var a = 23;
+
+/*************************************/
+
+var firstName = 'Angela';
+var age = 20;
+
+console.log(firstName + ' ' + age);
+
+var job , isSingle;
+job = 'Fight';
+isSingle = false;
+console.log(firstName + ' is a ' + age + ' year old ' + job + ' single ' + isSingle);
+
+age = 'one and two';
+job = 'Driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + ' single ' + isSingle);
+
+var lastName = prompt('what is last Name?:');
+console.log(firstName + ' ' + lastName);
+
+/*************************************/
+
+var yearNew, angela, Yu;
+yearNew = 2020;
+angela = yearNew - 10;
+Yu = yearNew - 20;
+console.log(angela)
+console.log(yearNew * 2);
+console.log(yearNew + 2);
+console.log(yearNew / 10);
+
+/*************************************/
+
+var angela = age > Yu;
+console.log(angela);
+
+/*************************************/
+
+console.log(typeof angela);
+console.log(typeof age);
+console.log(typeof 'Angela Yu');
+
+var x;
+console.log(typeof x);
+
+/*************************************/
+
+var now = 2020;
+var yearAngela = 1990;
+var fullAge = 19;
+
+var isFullAge = now - yearAngela >= fullAge;
+console.log(isFullAge);
+
