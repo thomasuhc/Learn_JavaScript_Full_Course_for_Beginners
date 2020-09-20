08f2fe5703411e791c27be9dec78ae0cb30af648
Thomas Chu <thomasuhc@gmail.com>
2020-09-19 22:16:07 -0400

    Final Chapter 3
    End exercise chapter 3...

--- 3_how_JS_works/starter/script.js
+++ 3_how_JS_works/starter/script.js
@@ -26,7 +26,7 @@ var age = 23;
 
 // First scoping example
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -39,13 +39,13 @@ function first() {
         console.log(a + b + c);
     }
 }
-*/
+
 
 
 
 // Example to show the differece between execution stack and scope chain
 
-/*
+
 var a = 'Hello!';
 first();
 
@@ -63,18 +63,41 @@ function third() {
     var d = 'John';
     console.log(a + b + c + d);
 }
-*/
 
 
 
 ///////////////////////////////////////
 // Lecture: The this keyword
 
+console.log(1999);
+
+function calculateAge(year) {
+
+    console.log( 2020 - year);
+    console.log(this);
+}
+
+var cintia = {
+    name : 'Cintia',
+    yearOfBirth: 1998,
+    calculateAge: function() {
+        console(this);
+        console.log(2020 - this.yearOfBirth);
+
+        function innerFunction () {
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+
+cintia.calculateAge();
 
 
+var bruna = {
+    name : 'Bruna',
+    yearOfBirth: 1998,
+    }
 
-
-
-
-
-
+bruna.calculateAge = cintia.calculateAge;
+bruna.calculateAge();
