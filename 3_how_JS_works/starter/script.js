95076787a2502b32e810f13d534c09e3d30b5166
Thomas Chu <thomasuhc@gmail.com>
2020-09-19 21:37:23 -0400

    New code JS
    Update chapter 3

---
+++ 3_how_JS_works/starter/script.js
@@ -0,0 +1,80 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+
+calculateAge(1999);
+
+function calculateAge (year) {
+
+    console.log(2020 - year);
+}
+
+
+var retirement = function (year) {
+    console.log(65 - (2020 -year));
+}
+
+console.log(age);
+var age = 23;
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
