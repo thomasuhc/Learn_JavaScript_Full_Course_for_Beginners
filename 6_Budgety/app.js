dc8b27398627baffdcd2ad658cd916cdc81ecd9a
Thomas Chu <thomasuhc@gmail.com>
2020-10-06 16:44:27 -0400

    Completed Updating the Budget
    Updating the Budget: Budget Controller

--- 6_Budgety/app.js
+++ 6_Budgety/app.js
@@ -5,13 +5,27 @@ var budgetController = (function(){
         this.id = id;
         this.description = description;
         this.value = value;
-    }
+    };
 
     var Income = function(id, description, value) {
 
         this.id = id;
         this.description = description;
         this.value = value;
+    };
+
+    var calculateTotal = function(type) {
+
+        var sum = 0;
+
+        data.allItems[type].forEach(function(cur){
+
+            sum = sum + cur.value;
+
+        });
+
+        data.totals[type] = sum;
+
     }
 
     var data = {
@@ -23,7 +37,9 @@ var budgetController = (function(){
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -52,6 +68,33 @@ var budgetController = (function(){
             data.allItems[type].push(newItem);
     
             return newItem;
+        },
+
+        calculateBudget: function() {
+
+            calculateTotal("exp");
+            calculateTotal("inc");
+    
+    
+            data.budget = data.totals.inc - data.totals.exp;
+    
+            if(data.totals.inc > 0) {
+                data.percentage =  Math.round((data.totals.exp / data.totals.inc) * 100);
+
+            } else {
+                data.percentage = -1;
+            }
+
+          
+        },
+
+        getBudget: function() {
+            return {
+                Budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
         }
 
     } 
@@ -158,7 +201,12 @@ var controller = (function(budgetCtrl, UICtrl){
 
     var updateBudget = function() {
 
+        budgetCtrl.calculateBudget();
 
+        var budget = budgetCtrl.getBudget();
+
+        console.log(budget);
+       
 
     };
    
