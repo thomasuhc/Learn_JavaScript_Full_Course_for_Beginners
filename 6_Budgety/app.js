6e4f684e7c0c546329222f4e8a863840f9cf7cb8
Thomas Chu <thomasuhc@gmail.com>
2020-10-07 15:07:57 -0400

    Completed Updating the Percentages UI Controller
    Updating the Percentages UI Controller

--- 6_Budgety/app.js
+++ 6_Budgety/app.js
@@ -170,7 +170,8 @@ var UIController = (function(){
         incomeLabel: ".budget__income--value",
         expensesLabel: ".budget__expenses--value",
         percentageLabel: ".budget__expenses--percentage",
-        container: ".container"
+        container: ".container",
+        expensesPercLabel: ".item__percentage"
         
     }
         return {
@@ -260,6 +261,29 @@ var UIController = (function(){
 
             },
 
+            displayPercentages: function(percentages) {
+
+                var fields = document.querySelectorAll(DOMStrings.expensesPercLabel);
+
+                var NodeListForEach = function(list, callback){
+                    for (var i = 0; i < list.length; i++){
+                        callback(list[i], i);
+                    }
+                };
+
+                NodeListForEach(fields, function(current, index){
+
+                    if(percentages[index] > 0){
+
+                        current.textContent = percentages[index] + "%";
+
+                    } else {
+                        current.textContent = "---";
+                    }
+
+                });
+            },
+
             getDOMStrings: function(){
                 return DOMStrings;
             }
@@ -307,7 +331,7 @@ var controller = (function(budgetCtrl, UICtrl){
 
       var percentages = budgetCtrl.getPercentages();
 
-      console.log(percentages);
+      UICtrl.displayPercentages(percentages);
 
     };
 
