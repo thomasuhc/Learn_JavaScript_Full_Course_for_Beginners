73ab808c865bb69f11e857ab080107d1cd28a45c
Thomas Chu <thomasuhc@gmail.com>
2020-10-08 15:32:12 -0400

    Completed exercises Arrow Functions 'this'
    Arrow Functions: Lexical 'this' Keyword

--- 7_ECMAScript6/script.js
+++ 7_ECMAScript6/script.js
@@ -139,3 +139,112 @@ ages6 = years.map((el, index) => {
 
 console.log(ages6);
 
+
+
+// Arrow Functions: Lexical 'this' Keyword
+
+// ES5
+
+var box5 = {
+
+    color: "green",
+    position: 1,
+    clickMe: function() {
+
+        var self = this;
+
+        document.querySelector(".green").addEventListener("click", function() {
+
+            var str = self.position + " " + self.color;
+
+            alert(str);
+            
+        });
+        
+    }
+}
+
+//box5.clickMe();
+
+// ES6
+
+const box6 = {
+
+    color: "green",
+    position: 1,
+    clickMe: function() {
+
+        document.querySelector(".green").addEventListener("click", () => {
+
+            var str = this.position + " " + this.color;
+
+            alert(str);
+            
+        });
+        
+    }
+}
+
+//box6.clickMe();
+
+const box66 = {
+
+    color: "green",
+    position: 1,
+    clickMe: () => {
+
+        document.querySelector(".green").addEventListener("click", () => {
+
+            var str = this.position + " " + this.color;
+
+            alert(str);
+            
+        });
+        
+    }
+}
+
+box66.clickMe();
+
+/////////////////////////////////////////////
+
+
+function Person(name) {
+
+    this.name = name;
+    
+} 
+
+// ES5
+
+
+Person.prototype.myFriends5 = function(friends) {
+
+    var arr = friends.map(function(el) {
+
+        return this.name + " oi " + el;
+        
+    }.bind(this));
+
+    console.log(arr);
+    
+}
+
+var friends = ["Ana", "Aline", "Miau"];
+
+new Person("Angela").myFriends5(friends);
+
+
+
+// ES6
+
+
+Person.prototype.myFriends6 = function(friends) {
+
+    var arr = friends.map(el => `${this.name} ${el}`);
+
+    console.log(arr);
+    
+}
+
+new Person("Serena").myFriends6(friends);
