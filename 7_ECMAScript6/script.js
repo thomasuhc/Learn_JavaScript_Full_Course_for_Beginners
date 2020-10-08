4f6be46db313428c0835a41f69322787c87ebc3d
Thomas Chu <thomasuhc@gmail.com>
2020-10-08 14:40:12 -0400

    Completed exercises Arrow Functions
    Arrow Functions basics

--- 7_ECMAScript6/script.js
+++ 7_ECMAScript6/script.js
@@ -104,3 +104,38 @@ console.log(n.startsWith('A'));
 console.log(n.endsWith('a'));
 console.log(n.includes('na'));
 console.log(`${firstName} `.repeat(5));
+
+//  Arrow Functions: Basics
+
+// ES5
+
+const years = [2000, 2005, 2010, 2015, 2020];
+
+var ages5 = years.map(function(el) {
+
+    return 2020 - el;    
+});
+
+console.log(ages5);
+
+
+// ES6
+
+let ages6 = years.map(el => 2020 - el);
+
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2020 - el}`);
+
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    
+    const now = new Date().getFullYear();
+    const age = now - el;
+
+    return `Age element ${index + 1}: ${age}`;
+});
+
+console.log(ages6);
+
